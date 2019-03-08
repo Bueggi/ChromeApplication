e0b02fb8d1ea051db57c24797d193143fdcfd19e
Christopher Bücklein <christopher_b@gmx.de>
2019-03-08 03:01:43 +0100

    played around with chrome extension

--- popup.js
+++ popup.js
@@ -1,15 +1,19 @@
-let changeColor = document.getElementById('changeColor');
+let changeColor = document.getElementById('badgeButton');
+let randomText = document.getElementById("randomText");
+let schreibWas = document.getElementById("schreibWas");
 
-  chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+  chrome.storage.sync.get('text', function({text}) {
+    randomText.innerHTML = text;
+    chrome.extension.getBackgroundPage().console.log(text);
   });
 
   changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
+    chrome.storage.sync.set({"text" : schreibWas.value});
+    randomText.innerHTML = schreibWas.value;
+    
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    //   chrome.tabs.executeScript(
+    //       tabs[0].id,
+    //       {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    // });
   };
